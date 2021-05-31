4c9bd270725f2c28592aca64e5112d016e1b2fa9
Trey <trey.husko@gmail.com>
2021-05-31 15:16:12 -0700

    Wrote second test, tests if year % 100 returns false

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -4,6 +4,7 @@ import LeapYear
 class testCaseLeapYear(unittest.TestCase):
     def testLeapYear(self):
         self.assertEqual(LeapYear.LeapYear(2004), True)
-
+    def testLeapYear2(self):
+        self.assertEqual(LeapYear.LeapYear(1900), False)
 if __name__ == "__main__":
     unittest.main()
