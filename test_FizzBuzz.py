db1bae086aa71e481732f72639aab24c84577dee
Trey <trey.husko@gmail.com>
2021-05-31 14:37:02 -0700

    Set up FizzBuzz function in FizzBuzz. Wrote first test for multiples of 3 to print Fizz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -1,2 +1,9 @@
 import FizzBuzz
 import unittest
+
+class testCaseFizzBuzz(unittest.TestCase):
+    def test_FizzBuzz(self):
+        self.assertEqual(FizzBuzz.FizzBuzz(3), "Fizz")
+
+if __name__ == "__main__":
+    unittest.main()
