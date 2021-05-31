9f2de5a07202c21d1749b952e0432cc21e706ccb
Trey <trey.husko@gmail.com>
2021-05-31 15:20:10 -0700

    Wrote third test, tests if year % 400 returns true if == 0.

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -6,5 +6,7 @@ class testCaseLeapYear(unittest.TestCase):
         self.assertEqual(LeapYear.LeapYear(2004), True)
     def testLeapYear2(self):
         self.assertEqual(LeapYear.LeapYear(1900), False)
+    def testLeapYear3(self):
+        self.assertEqual(LeapYear.LeapYear(2000), True)
 if __name__ == "__main__":
     unittest.main()
