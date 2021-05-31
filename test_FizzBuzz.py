8d2f26a5fcdd431ff3bb3c1b60d1f4eafbc60c0b
Trey <trey.husko@gmail.com>
2021-05-31 14:56:26 -0700

    Forgot 20 is multiple of 5, changed 20 to 7 in test

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -9,7 +9,7 @@ class testCaseFizzBuzz(unittest.TestCase):
     def test_FizzBuzz3(self):
         self.assertEqual(FizzBuzz.FizzBuzz(15), "FizzBuzz")
     def test_FizzBuzz4(self):
-        self.assertEqual(FizzBuzz.FizzBuzz(20), 20)
+        self.assertEqual(FizzBuzz.FizzBuzz(7), 7)
 
 if __name__ == "__main__":
     unittest.main()
