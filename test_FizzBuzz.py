fdf84c33e8277efdd124b4a7a24d0fd62758dfe1
Trey <trey.husko@gmail.com>
2021-05-31 14:44:06 -0700

    Wrote second test, tests multiples of 5 to return Buzz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -4,6 +4,8 @@ import unittest
 class testCaseFizzBuzz(unittest.TestCase):
     def test_FizzBuzz(self):
         self.assertEqual(FizzBuzz.FizzBuzz(3), "Fizz")
+    def test_FizzBuzz2(self):
+        self.assertEqual(FizzBuzz.FizzBuzz(5), "Buzz")
 
 if __name__ == "__main__":
     unittest.main()
