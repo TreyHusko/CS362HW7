dbc6e6402397fee2a16097b8875d9d3f7a15534d
Trey <trey.husko@gmail.com>
2021-05-31 14:48:22 -0700

    Wrote third test, tests if number is a multiple of 3 and 5 and returns FizzBuzz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -6,6 +6,8 @@ class testCaseFizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz.FizzBuzz(3), "Fizz")
     def test_FizzBuzz2(self):
         self.assertEqual(FizzBuzz.FizzBuzz(5), "Buzz")
+    def test_FizzBuzz3(self):
+        self.assertEqual(FizzBuzz.FizzBuzz(15), "FizzBuzz")
 
 if __name__ == "__main__":
     unittest.main()
